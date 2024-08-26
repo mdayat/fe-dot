58ed2ae8f836c1db3758aa4ad32fed0926ebcae7
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2024-08-26 18:18:04 +0700

    remove unnecessary codes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
+import Head from "next/head";
 import type { AppProps } from "next/app";
 
+import "../styles/globals.css";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
